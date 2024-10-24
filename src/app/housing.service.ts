66a474881094722ef1a24639eb8b1d06e2e2da83
suraj-i2e <suraj.singh@i2econsulting.com>
2024-10-24 11:40:22 +0530

    json url update

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -5,7 +5,7 @@ import { HousingLocation } from './housing-location';
   providedIn: 'root'
 })
 export class HousingService {
-  url='http://localhost:3000/locations'
+  url='https://suraj-i2e.github.io/homes_api/db.json/locations'
 
   constructor() { }
 
